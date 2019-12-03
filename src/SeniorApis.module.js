67cbe21a6285fc9ed222ae09b3520d20b5cda097
Raúl Ortega <raul.ortega@seniortelecom.es>
2019-12-03 16:44:06 +0100

    Modificamos url basica de inicio video

--- src/SeniorApis.module.js
+++ src/SeniorApis.module.js
@@ -30,7 +30,7 @@ RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 function RoutesConfig($stateProvider, $urlRouterProvider) {
 
   // Redirect to tab 1 if no other URL matches
-  $urlRouterProvider.otherwise('/profileVideo');
+  $urlRouterProvider.otherwise('');
 
   // Set up UI states
   $stateProvider
